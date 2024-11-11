7d5ba12db27e7f9e26e36cf344a73358c3ded2b2
Durjoy96 <mddurjoy853@gmail.com>
2024-11-11 23:27:28 +0600

    skills section changed

--- src/components/About/Skill.jsx
+++ src/components/About/Skill.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const Skill = ({ skill }) => {
   const { software, icon } = skill;
   return (
-    <div className="p-5 rounded-xl bg-base-100 border">
-      <div className="w-24 h-24 mx-auto">
-        <img className="w-full h-full object-contain" src={icon} alt="" />
+    <div className="flex items-center gap-6">
+      <div className="w-10 h-10 border">
+        <img
+          className="w-full h-full object-contain grayscale"
+          src={icon}
+          alt=""
+        />
       </div>
-      <p className="text-lg font-bold text-base-content mt-3">{software}</p>
+      <h4 className="text-lg font-bold text-base-content">{software}</h4>
     </div>
   );
 };
