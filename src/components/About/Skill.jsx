985d8d4dc6bcdb6e1cc1c49bec8e7a2adeb50457
Durjoy96 <mddurjoy853@gmail.com>
2024-11-11 23:53:01 +0600

    skills section improved

--- src/components/About/Skill.jsx
+++ src/components/About/Skill.jsx
@@ -3,15 +3,17 @@ import React from "react";
 const Skill = ({ skill }) => {
   const { software, icon } = skill;
   return (
-    <div className="flex items-center gap-6">
+    <div className="flex items-center gap-6 group cursor-default">
       <div className="w-10 h-10 border">
         <img
-          className="w-full h-full object-contain grayscale"
+          className="w-full h-full object-contain grayscale group-hover:grayscale-0"
           src={icon}
           alt=""
         />
       </div>
-      <h4 className="text-lg font-bold text-base-content">{software}</h4>
+      <h4 className="text-lg font-bold text-base-content group-hover:text-primary">
+        {software}
+      </h4>
     </div>
   );
 };
