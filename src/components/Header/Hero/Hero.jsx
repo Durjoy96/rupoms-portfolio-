2b72f8cd9088632d188e1e8341ed946e7baad56b
Durjoy96 <mddurjoy853@gmail.com>
2024-11-12 21:03:35 +0600

    hero section unusual problem solved

--- src/components/Header/Hero/Hero.jsx
+++ src/components/Header/Hero/Hero.jsx
@@ -23,9 +23,9 @@ const Hero = () => {
               <Button text="Download CV" isPrimary={false}></Button>
             </div>
           </div>
-          <div className="w-full">
+          <div className="w-full lg:w-auto">
             <iframe
-              className="mx-auto rounded-xl skeleton h-[200px] w-full shadow-lg md:h-[350px] md:w-[550px]"
+              className="mx-auto rounded-xl skeleton h-[200px] w-full shadow-lg md:h-[350px] md:w-[550px] lg:mx-0"
               src="https://www.youtube.com/embed/ijw_1AuCTes?si=4IeHBeMgyecHbDBc?rel=0"
               title="YouTube video player"
               frameborder="0"
