bc71b03a4fd2e0a54942599c588532cfce65ce93
Durjoy96 <mddurjoy853@gmail.com>
2025-09-30 00:35:08 +0600

    theme change to cupcake

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,7 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="cupcake">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-base-100`}
       >
